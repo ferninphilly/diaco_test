1d44917704e4ff2ffe71952b274f9669d22e9d4d
Fernando Pombeiro <fernandopombeiro@Fernandos-MacBook-Pro.local>
2017-07-06 21:31:13 -0600

    finished the front end

--- diaco_app/diaco_app.py
+++ diaco_app/diaco_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 from bs4 import BeautifulSoup
 import requests
 
@@ -18,24 +18,29 @@ def grab_data(url, reviews=10):
     if page.status_code == 200:
         yelp_data = BeautifulSoup(page.content, 'html.parser')
         all_revs = yelp_data.find_all("div", class_="review-content")
-        review_lst = list()
+        star_div = yelp_data.find("div", class_="rating-info clearfix").find('img')
+        review_lst = [star_div.get('alt', '')]
         for each in all_revs:
             inner = BeautifulSoup(str(each), 'html.parser')
-            review_lst.append(inner.find('p'))
-        return review_lst[0:reviews]
+            review_lst.append(inner.find('p').get_text())
     else:
-        review_lst.append("Sorry but there was an error in retrieving the restaurant name."\
-        "Are you sure you spelled it right?")
-        return review_lst
-
-
-
-
-
+        review_lst = ["Error: Sorry but there was an error in retrieving the restaurant name."\
+        "Are you sure you spelled it right?"]
+    return review_lst[0:int(reviews)]
 
 
 @app.route("/")
-def hello():
+def homepage():
     return render_template('layout.html')
 
-#@app.route("/<restaurant>", methods=[GET])
+@app.route("/result", methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        reviews = request.form['reviews']
+        restaurant = request.form['restaurant']
+        url = url_creator(restaurant)
+        result = (restaurant, grab_data(url, int(reviews)))
+        return render_template("result.html", result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
