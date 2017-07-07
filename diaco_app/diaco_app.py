43b9d8185d47149bf9ef8d944fab15f43922557f
Fernando Pombeiro <fernandopombeiro@Fernandos-MacBook-Pro.local>
2017-07-06 22:28:08 -0600

    Getting rid of old stuff

--- diaco_app/diaco_app.py
+++ diaco_app/diaco_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request
 from bs4 import BeautifulSoup
+import statistics
 import requests
 
 app = Flask(__name__)
@@ -9,8 +10,8 @@ def url_creator(restaurant_name):
     lower_restaurant = restaurant_name.lower()
     replace_restaurant = lower_restaurant.replace("'", "")
     restaurant=replace_restaurant.replace(" ", "-")
-    #create URL. TODO: add some different cities
-    url="https://www.yelp.com/biz/" + restaurant + "-new-york"
+    #create URL. Order by most recent per instructions
+    url="https://www.yelp.com/biz/" + restaurant + "-new-york?sort_by=date_desc"
     return url
 
 def grab_data(url, reviews=10):
@@ -18,15 +19,17 @@ def grab_data(url, reviews=10):
     if page.status_code == 200:
         yelp_data = BeautifulSoup(page.content, 'html.parser')
         all_revs = yelp_data.find_all("div", class_="review-content")
-        star_div = yelp_data.find("div", class_="rating-info clearfix").find('img')
-        review_lst = [star_div.get('alt', '')]
+        review_lst = list()
+        stars = list()
         for each in all_revs:
             inner = BeautifulSoup(str(each), 'html.parser')
             review_lst.append(inner.find('p').get_text())
+            rev_num = int(inner.find('img').get('alt')[:1])
+            stars.append(rev_num)
     else:
         review_lst = ["Error: Sorry but there was an error in retrieving the restaurant name."\
         "Are you sure you spelled it right?"]
-    return review_lst[0:int(reviews)]
+    return review_lst[0:int(reviews)], stars[0:int(reviews)]
 
 
 @app.route("/")
@@ -39,7 +42,8 @@ def result():
         reviews = request.form['reviews']
         restaurant = request.form['restaurant']
         url = url_creator(restaurant)
-        result = (restaurant, grab_data(url, int(reviews)))
+        reviews, stars = grab_data(url, int(reviews))
+        result = (restaurant, reviews, statistics.mean(stars), len(stars))
         return render_template("result.html", result=result)
 
 if __name__ == '__main__':
